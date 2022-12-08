023e88a458889faa5d07c2d8e5f465a2e518e32f
Vasilevykh-M <vasilevyh00@gmail.com>
2022-12-08 16:39:14 +0500

    fix_request

--- app/modules/dictionary/api/v1/dictionary.py
+++ app/modules/dictionary/api/v1/dictionary.py
@@ -3,37 +3,40 @@ from sqlalchemy.orm import Session
 
 from app.modules.dictionary.models import Language, Book, Author
 from app.common.db import get_db
-
+from fastapi.middleware.cors import CORSMiddleware
 
 router = APIRouter()
+
 
 
 # получить всех авторов
 @router.get("/Authors")
 def read_root(session: Session = Depends(get_db)):
     try:
-        return [
+        return {"status": "", "result":[
             {
-                "ФИО": x.fio,
-                "Дата рождения": x.birthday,
-                "Биография": x.biography
+                "id": x.id,
+                "FIO": x.fio,
+                "Birthday": x.birthday,
+                "Biography": x.biography
             } for x in session.query(Author)
-        ]
+        ]}
     except:
-        return {"status": "no data to return"}
+        return {"status": "no data to return", "result": []}
 
 
 # получить все языки
 @router.get("/Languages")
 def read_root(session: Session = Depends(get_db)):
     try:
-        return [
+        return {"status": "", "result":[
             {
-                "Язык": x.language,
+                "id": x.id,
+                "Language": x.language,
             } for x in session.query(Language)
-        ]
+        ]}
     except:
-        return {"status": "no data to return"}
+        return {"status": "no data to return", "result": []}
 
 
 # получить все книги
@@ -41,44 +44,47 @@ def read_root(session: Session = Depends(get_db)):
 def read_root(session: Session = Depends(get_db)):
 
     try:
-        return [
+        return {"status": "", "result":[
             {
-                "Книга": x.Book.name_book,
-                "Дата издания": x.Book.year_book,
-                "Язык": x.Language.language,
-                "Автор": x.Author.fio,
-                "Прочитана": x.Book.state_read
+                "id": x.Book.id,
+                "NameBook": x.Book.name_book,
+                "YearBook": x.Book.year_book,
+                "Language": x.Language.language,
+                "Author": x.Author.fio,
+                "StateRead": x.Book.state_read
             } for x in session.query(Book, Language, Author).join(Language).join(Author)
-        ]
+        ]}
     except:
-        return {"status": "no data to return"}
+        return {"status": "no data to return", "result": []}
 
 
 # поиск книг данного автора (на любом языке или на конкретном, среди прочитанных, среди планируемых к прочтению)
 # если значение id языка дефолтное (-1) то читай книги на всех языках, дефолтное значение для чтения = Не прочитана
 @router.get("/authors_book")
-def read_root(id: int, id_languages :int = -1, read: bool = False, session: Session = Depends(get_db)):
-    books = session.query(Book, Language, Author).join(Language).join(Author).filter(Book.id_author == id, Book.state_read == read)
-    if id_languages != -1:
+def read_root(fio: str, language :str = "", read: bool = False, session: Session = Depends(get_db)):
+    author = session.query(Author).filter(Author.fio == fio).first()
+    books = session.query(Book, Language, Author).join(Language).join(Author).filter(Book.id_author == author.id, Book.state_read == read)
+    if language != "":
 
-        languag = session.query(Language).filter(Language.id == id_languages).first()
+        languag = session.query(Language).filter(Language.language == language).first()
 
         if languag == None:
-            return {"status": "no data to return"}
+            return {"status": "no data to return", "result": []}
 
-        books = books.filter(Book.id_language == id_languages)
+        books = books.filter(Book.id_language == languag.id)
     try:
-        return [
+        return {"status": "", "result":[
             {
-                "Книга": x.Book.name_book,
-                "Дата издания": x.Book.year_book,
-                "Язык": x.Language.language,
-                "Автор": x.Author.fio,
-                "Прочитана": x.Book.state_read
+                "id": x.id,
+                "NameBook": x.Book.name_book,
+                "YearBook": x.Book.year_book,
+                "Language": x.Language.language,
+                "Author": x.Author.fio,
+                "StateRead": x.Book.state_read
             } for x in books
-        ]
+        ]}
     except:
-        return {"status": "no data to return"}
+        return {"status": "no data to return", "result": []}
 
 
 # поиск всех прочитанных книг;
@@ -87,89 +93,92 @@ def read_root(session: Session = Depends(get_db)):
     books = session.query(Book, Language, Author).join(Language).join(Author).filter(Book.state_read == True)
 
     try:
-        return [
+        return {"status": "", "result":[
             {
-                "Книга": x.Book.name_book,
-                "Дата издания": x.Book.year_book,
-                "Язык": x.Language.language,
-                "Автор": x.Author.fio,
-                "Прочитана": x.Book.state_read
+                "NameBook": x.Book.name_book,
+                "YearBook": x.Book.year_book,
+                "Language": x.Language.language,
+                "Author": x.Author.fio,
+                "StateRead": x.Book.state_read
             } for x in books
-        ]
+        ]}
     except:
-        return {"status": "no data to return"}
+        return {"status": "no data to return", "result": []}
 
 
 # поиск всех планируемых к прочтению книг (на любом языке или конкретном)
 # если значение id языка дефолтное (-1) то читай книги на всех языках, дефолтное значение для чтения = Не прочитана
 @router.get("/not_read_book")
-def read_root(id_languages :int = -1, session: Session = Depends(get_db)):
+def read_root(languages :str, session: Session = Depends(get_db)):
     books = session.query(Book, Language, Author).join(Language).join(Author).filter(Book.state_read == False)
 
-    if id_languages != -1:
+    if languages != "":
 
-        languag = session.query(Language).filter(Language.id == id_languages).first()
+        languag = session.query(Language).filter(Language.language == languages).first()
 
         if languag == None:
-            return {"status": "no data to return"}
+            return {"status": "no data to return", "result": []}
 
-        books = books.filter(Book.id_language == id_languages)
+        books = books.filter(Book.id_language == languag.id)
 
     try:
-        return [
+        return {"status": "", "result":[
             {
-                "Книга": x.Book.name_book,
-                "Дата издания": x.Book.year_book,
-                "Язык": x.Language.language,
-                "Автор": x.Author.fio,
-                "Прочитана": x.Book.state_read
+                "NameBook": x.Book.name_book,
+                "YearBook": x.Book.year_book,
+                "Language": x.Language.language,
+                "Author": x.Author.fio,
+                "StateRead": x.Book.state_read
             } for x in books
-        ]
+        ]}
     except:
-        return {"status": "no data to return"}
+        return {"status": "no data to return", "result": []}
 
 
 #  поиск автора по имени
 @router.get("/author")
-def read_root(fio :str, session: Session = Depends(get_db)):
-    author = session.query(Author).filter(Author.fio == fio)
+def read_root(id: int, session: Session = Depends(get_db)):
+    author = session.query(Author).filter(Author.id == id)
     try:
-        return [
+        return {"status": "", "result":[
             {
-                "ФИО": x.fio,
-                "Дата рождения": x.birthday,
-                "Биография": x.biography
+                "id": x.id,
+                "FIO": x.fio,
+                "Birthday": x.birthday,
+                "Biography": x.biography
             } for x in author
-        ]
+        ]}
     except:
-        return {"status": "no data to return"}
+        return {"status": "no data to return", "result": []}
 
 
 # поиск книги по названию
 @router.get("/book")
-def read_root(name_book :str, session: Session = Depends(get_db)):
-    book = session.query(Book, Language, Author).join(Language).join(Author).filter(Book.name_book == name_book)
+def read_root(id :int, session: Session = Depends(get_db)):
+    book = session.query(Book, Language, Author).join(Language).join(Author).filter(Book.id == id)
     try:
-        return [
+        return {"status": "", "result":[
             {
-                "Книга": x.Book.name_book,
-                "Дата издания": x.Book.year_book,
-                "Язык": x.Language.language,
-                "Автор": x.Author.fio,
-                "Прочитана": x.Book.state_read
+                "id": x.Book.id,
+                "NameBook": x.Book.name_book,
+                "YearBook": x.Book.year_book,
+                "Language": x.Language.language,
+                "Author": x.Author.fio,
+                "StateRead": x.Book.state_read
             } for x in book
-        ]
+        ]}
     except:
-        return {"status": "no data to return"}
+        return {"status": "no data to return", "result": []}
+
 
 # добавить ии изменить язык
 # если язык есть изменить его имя, если нет то создать
 # если id языка нет то мы его создаем иначе изменяем его поля
 @router.post("/languages/add")
-def read_item(id: int, language: str, session: Session = Depends(get_db)):
+def read_item(id: int, language: str,  session: Session = Depends(get_db)):
     languag = session.query(Language).filter(Language.id == id).first()
     if languag == None:
-        new_language = Language(id = id, language = language)
+        new_language = Language(language = language)
         session.add(new_language)
     else:
         languag.language = language
@@ -182,18 +191,13 @@ def read_item(id: int, language: str, session: Session = Depends(get_db)):
 # создать или изменит автора
 # по дефолту поэтому можно указывать не все параметры
 # если id автора нет то мы его создаем иначе изменяем его поля
-@router.post("/authors/add")
-def read_item(id: int, fio="", birthday="", biography="", session: Session = Depends(get_db)):
+@router.post("/authors/mod")
+def read_item(id: int, fio:str , birthday="", biography="", session: Session = Depends(get_db)):
     author = session.query(Author).filter(Author.id == id).first()
     if author == None:
-        if fio!="":
-            new_author = Author(id = id, fio = fio, birthday = birthday, biography = biography)
-            session.add(new_author)
-        else:
-            return {"status": "no have all need data"}
+        return {"status": "no have all need data"}
     else:
-        if fio!="":
-            author.fio = fio
+        author.fio = fio
         if birthday!="":
             author.birthday = birthday
         if biography!="":
@@ -202,46 +206,34 @@ def read_item(id: int, fio="", birthday="", biography="", session: Session = Dep
 
     return {"status": "OK"}
 
+@router.put("/authors/add")
+def read_item(fio:str , birthday="", biography="", session: Session = Depends(get_db)):
+    new_author = Author(fio = fio, birthday = birthday, biography = biography)
+    session.add(new_author)
+    session.commit()
+
+    return {"status": "OK"}
 
 # создать или изменить книгу
 # если книга есть то мы обязаны изменить её состояние (можем оставить на том же)
 # если id книги нет то мы его создаем иначе изменяем его поля
-@router.post("/books/add")
-def read_item(id: int, state_read: bool, name_book="", year_book ="", id_language:int= -1, id_author:int = -1, session: Session = Depends(get_db)):
+@router.post("/books/mod")
+def read_item(id: int, state_read: bool, name_book, year_book ="", language:str= "", fio:str = "", session: Session = Depends(get_db)):
     book = session.query(Book).filter(Book.id == id).first()
     if book == None:
-        language = session.query(Language).filter(Language.id == id_language)
-        author = session.query(Author).filter(Author.id == id_author)
-
-        if language.first() == None or author.first() == None:
-            return {"status": "no have all need data"}
-
-        if name_book!="":
-            new_book = Book(
-                id = id,
-                name_book = name_book,
-                year_book = year_book,
-                id_language = id_language,
-                id_author = id_author,
-                state_read = state_read
-            )
-            session.add(new_book)
-            session.commit()
-            return {"status": "OK"}
-        else:
-            return {"status": "no have all need data"}
+        return {"status": "no have all need data"}
     else:
-        if id_language != -1:
-            language = session.query(Language).filter(Language.id == id_language)
-            if language.first() == None:
+        if language != "":
+            languages = session.query(Language).filter(Language.language == language)
+            if languages.first() == None:
                 return {"status": "no have all need data"}
-            book.id_language = id_language
+            book.id_language = languages.first().id
 
-        if id_author != -1:
-            author = session.query(Author).filter(Author.id == id_author)
+        if fio != "":
+            author = session.query(Author).filter(Author.fio == fio)
             if author.first() == None:
                 return {"status": "no have all need data"}
-            book.id_author = id_author
+            book.id_author = author.first().id
 
         if name_book != "":
             book.name_book = name_book
@@ -253,9 +245,30 @@ def read_item(id: int, state_read: bool, name_book="", year_book ="", id_languag
         return {"status": "OK"}
 
 
+
+
+@router.put("/books/add")
+def read_item(state_read: bool, name_book, year_book ="", language:str= "", fio:str = "", session: Session = Depends(get_db)):
+    languages = session.query(Language).filter(Language.language == language)
+    author = session.query(Author).filter(Author.fio == fio)
+
+    if languages.first() == None or author.first() == None:
+        return {"status": "no have all need data"}
+
+    new_book = Book(
+                name_book = name_book,
+                year_book = year_book,
+                id_language = languages.first().id,
+                id_author = author.first().id,
+                state_read = state_read
+        )
+    session.add(new_book)
+    session.commit()
+    return {"status": "OK"}
+
 # удаление автора по id
 @router.delete("/authors/delete")
-def read_item(id: int,session: Session = Depends(get_db)):
+def read_item(id: int, session: Session = Depends(get_db)):
     author = session.query(Author).filter(Author.id == id).first()
     if author:
         session.delete(author)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 
 from app import create_app
+from fastapi.middleware.cors import CORSMiddleware
 
 
 load_dotenv(verbose=True, dotenv_path=".env.local")
@@ -15,3 +16,15 @@ if __name__ == '__main__':
                 log_level="info", reload=True, reload_dirs=["app"])
 else:
     app = create_app()
+    origins = [
+        "http://localhost",
+        "http://localhost:3000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
